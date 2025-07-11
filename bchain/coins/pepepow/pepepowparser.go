841976de6230a98bfb7167e7abeefa8abf99009c
Edison Huang <107197406+edisontw@users.noreply.github.com>
2025-07-11 19:36:32 +0800

    Update pepepowparser.go

--- bchain/coins/pepepow/pepepowparser.go
+++ bchain/coins/pepepow/pepepowparser.go
@@ -1,63 +1,27 @@
 package pepepow
 
 import (
-	"github.com/martinboehm/btcd/wire"
-	"github.com/martinboehm/btcutil/chaincfg"
-	"github.com/trezor/blockbook/bchain/coins/btc"
+    "github.com/trezor/blockbook/bchain"
+    "github.com/trezor/blockbook/bchain/coins/dash"
 )
 
-// magic numbers
-const (
-	MainnetMagic wire.BitcoinNet = 0xf1e2d3c4
-	TestnetMagic wire.BitcoinNet = 0x0709110b
-)
-
-// chain parameters
-var (
-	MainNetParams chaincfg.Params
-	TestNetParams chaincfg.Params
-)
-
+// init registers the PepePow parser using the Dash parser as base.
 func init() {
-	MainNetParams = chaincfg.MainNetParams
-	MainNetParams.Net = MainnetMagic
-	MainNetParams.PubKeyHashAddrID = []byte{55}
-	MainNetParams.ScriptHashAddrID = []byte{117}
-	MainNetParams.Bech32HRPSegwit = "pepe"
-
-	TestNetParams = chaincfg.TestNet3Params
-	TestNetParams.Net = TestnetMagic
-	TestNetParams.PubKeyHashAddrID = []byte{111}
-	TestNetParams.ScriptHashAddrID = []byte{196}
-	TestNetParams.Bech32HRPSegwit = "tpepe"
+    bchain.RegisterParser("pepepow", parsePepePowParams)
 }
 
-// PepepowParser handle
-type PepepowParser struct {
-	*btc.BitcoinLikeParser
-}
+// parsePepePowParams delegates to Dash parser because PepePow is a fork of Dash core v0.12.2.
+// It then overrides any chain-specific parameters from the raw configuration.
+func parsePepePowParams(raw *bchain.RawChaincfg) (*bchain.Chaincfg, error) {
+    // Use the Dash parser for most settings
+    cfg, err := dash.Parse(raw)
+    if err != nil {
+        return nil, err
+    }
 
-// NewPepepowParser returns new PepepowParser instance
-func NewPepepowParser(params *chaincfg.Params, c *btc.Configuration) *PepepowParser {
-	return &PepepowParser{BitcoinLikeParser: btc.NewBitcoinLikeParser(params, c)}
-}
+    // Override XPub magic values specific to PepePow
+    cfg.XpubMagic = raw.XpubMagic
+    cfg.XpubMagicSegwitP2sh = raw.XpubMagicSegwitP2sh
+    cfg.XpubMagicSegwitNative = raw.XpubMagicSegwitNative
 
-// GetChainParams contains network parameters for the main Pepepow network,
-// and the test Pepepow network
-func GetChainParams(chain string) *chaincfg.Params {
-	if !chaincfg.IsRegistered(&MainNetParams) {
-		err := chaincfg.Register(&MainNetParams)
-		if err == nil {
-			err = chaincfg.Register(&TestNetParams)
-		}
-		if err != nil {
-			panic(err)
-		}
-	}
-	switch chain {
-	case "test":
-		return &TestNetParams
-	default:
-		return &MainNetParams
-	}
-}
+    return cfg,
