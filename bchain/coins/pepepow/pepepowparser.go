741c90518ea17a8fb4272d5f999a6669cb29abd1
Edison Huang <107197406+edisontw@users.noreply.github.com>
2025-10-31 00:21:57 +0800

    Implement PepePow parser (#2)

--- bchain/coins/pepepow/pepepowparser.go
+++ bchain/coins/pepepow/pepepowparser.go
@@ -2,73 +2,60 @@ package pepepow
 
 import (
 	"github.com/martinboehm/btcutil/chaincfg"
+	"github.com/trezor/blockbook/bchain"
 	"github.com/trezor/blockbook/bchain/coins/btc"
 	"github.com/trezor/blockbook/bchain/coins/dash"
 )
 
 const (
-	// MainnetMagic mirrors Dash's magic constant for network detection.
-	MainnetMagic = dash.MainnetMagic
-	// TestnetMagic mirrors Dash's testnet magic constant.
-	TestnetMagic = dash.TestnetMagic
-	// RegtestMagic mirrors Dash's regtest magic constant.
-	RegtestMagic = dash.RegtestMagic
+	// xpub defaults copied from Bitcoin as PepePow reuses the same serialization.
+	xpubMagic             uint32 = 0x0488b21e
+	xpubMagicSegwitP2sh   uint32 = 0x049d7cb2
+	xpubMagicSegwitNative uint32 = 0x04b24746
 )
 
-var (
-	mainNetParams chaincfg.Params
-	testNetParams chaincfg.Params
-	regtestParams chaincfg.Params
-)
-
-func init() {
-	mainNetParams = *dash.GetChainParams("main")
-	mainNetParams.Name = "Pepepow"
-	mainNetParams.Net = MainnetMagic
-	mainNetParams.PubKeyHashAddrID = []byte{55} // base58 prefix: P
-	mainNetParams.ScriptHashAddrID = []byte{56} // base58 prefix: 3
-
-	testNetParams = *dash.GetChainParams("test")
-	testNetParams.Name = "Pepepow testnet"
-	testNetParams.Net = TestnetMagic
-
-	regtestParams = *dash.GetChainParams("regtest")
-	regtestParams.Name = "Pepepow regtest"
-	regtestParams.Net = RegtestMagic
+// PepepowParser implements the bitcoin-like parser logic for PepePow.
+type PepepowParser struct {
+	*btc.BitcoinLikeParser
+	baseparser *bchain.BaseParser
 }
 
-// GetChainParams returns the Pepepow-specific chain parameters for the selected network.
+func applyPepepowConfig(c *btc.Configuration) *btc.Configuration {
+	if c == nil {
+		c = &btc.Configuration{}
+	}
+	if c.XPubMagic == 0 {
+		c.XPubMagic = xpubMagic
+	}
+	if c.XPubMagicSegwitP2sh == 0 {
+		c.XPubMagicSegwitP2sh = xpubMagicSegwitP2sh
+	}
+	if c.XPubMagicSegwitNative == 0 {
+		c.XPubMagicSegwitNative = xpubMagicSegwitNative
+	}
+	return c
+}
+
+// NewPepepowParser returns a new parser instance configured for PepePow.
+func NewPepepowParser(params *chaincfg.Params, c *btc.Configuration) *PepepowParser {
+	cfg := applyPepepowConfig(c)
+	return &PepepowParser{
+		BitcoinLikeParser: btc.NewBitcoinLikeParser(params, cfg),
+		baseparser:        &bchain.BaseParser{},
+	}
+}
+
+// GetChainParams proxies to the Dash chain parameters as PepePow inherits them.
 func GetChainParams(chain string) *chaincfg.Params {
-	if !chaincfg.IsRegistered(&mainNetParams) {
-		if err := chaincfg.Register(&mainNetParams); err != nil && err != chaincfg.ErrDuplicateNet {
-			panic(err)
-		}
-		if err := chaincfg.Register(&testNetParams); err != nil && err != chaincfg.ErrDuplicateNet {
-			panic(err)
-		}
-		if err := chaincfg.Register(&regtestParams); err != nil && err != chaincfg.ErrDuplicateNet {
-			panic(err)
-		}
-	}
-
-	switch chain {
-	case "test":
-		return &testNetParams
-	case "regtest":
-		return &regtestParams
-	default:
-		return &mainNetParams
-	}
+	return dash.GetChainParams(chain)
 }
 
-// NewPepepowParser creates a Dash-compatible parser with Pepepow specific overrides applied.
-func NewPepepowParser(params *chaincfg.Params, cfg *btc.Configuration) (*dash.DashParser, error) {
-	parser := dash.NewDashParser(params, cfg)
+// PackTx packs transaction to byte array using protobuf.
+func (p *PepepowParser) PackTx(tx *bchain.Tx, height uint32, blockTime int64) ([]byte, error) {
+	return p.baseparser.PackTx(tx, height, blockTime)
+}
 
-	parser.BitcoinLikeParser.XPubMagic = cfg.XPubMagic
-	parser.BitcoinLikeParser.XPubMagicSegwitP2sh = cfg.XPubMagicSegwitP2sh
-	parser.BitcoinLikeParser.XPubMagicSegwitNative = cfg.XPubMagicSegwitNative
-	parser.BitcoinLikeParser.Slip44 = cfg.Slip44
-
-	return parser, nil
+// UnpackTx unpacks transaction from protobuf byte array.
+func (p *PepepowParser) UnpackTx(buf []byte) (*bchain.Tx, uint32, error) {
+	return p.baseparser.UnpackTx(buf)
 }
